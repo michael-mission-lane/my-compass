8532282de20bfbbcbb44853420e658b6eb4d632f
Michael O'Sullivan <michael.osullivan@missionlane.com>
2022-08-18 16:14:35 -0600

    cleanup, revert to 17

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,16 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { P1 } from '@missionlane/compass-ui';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
+        <P1>
           Edit <code>src/App.tsx</code> and save to reload.
-        </p>
+        </P1>
         <a
           className="App-link"
           href="https://reactjs.org"
